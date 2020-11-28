a7f92832e67096c99d5ad6ea837d5eb392f38cef
Cosimo Russo <cosimo.russo123@gmail.com>
2020-11-28 15:12:48 +0100

    Automatically redirect from home to dashboard if user is logged in

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, {useContext} from "react";
 import Navbar from "./components/Navbar";
+import { Redirect } from 'react-router-dom';
+
+const {UserContext} = require('./utils/user-context');
+
 const Home = props => {
+  const { user } = useContext(UserContext);
+
+  if(user) return <Redirect to={'/dashboard'} />
+
   return <div>
     <Navbar/>
     home page</div>;
