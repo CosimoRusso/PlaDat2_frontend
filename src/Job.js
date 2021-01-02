f2cdb211b18248df2541397099148468b798e178
Cosimo Russo <cosimo.russo123@gmail.com>
2021-01-02 22:22:48 +0100

    Make job component use functional style

--- src/Job.js
+++ src/Job.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import Carousel from 'react-elastic-carousel';
 import Card from './components/Card';
 import './job.css';
-import { withStyles } from '@material-ui/core/styles';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import { Typography } from '@material-ui/core';
@@ -13,11 +13,12 @@ import {UserContext} from './utils/user-context';
 import ModalViewJob from './components/ModalViewJob';
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
 import history from "./history";
+import {useSnackbar} from "notistack";
 
 const {get, post} = utils;
 
 
-const useStyles = theme => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         width: '90%',
         flexGrow: 1,
@@ -61,7 +62,7 @@ const useStyles = theme => ({
         color: '#1076a3',
     }
 
-});
+}));
 
 
 const breakPoints = [
@@ -88,128 +89,126 @@ const placeHolderText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit
     "ullamcorper condimentum venenatis. Suspendisse metus felis," +
     "laoreet hendrerit vehicula ac, lacinia a enim."
 
-class CardCarousel extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            similarJobs: [],
-            applied: false
-        }
-    }
-    static contextType = UserContext;
-    componentDidMount(){
+export default function CardCarousel(props){
+    const [similarJobs, setSimilarJobs] = useState([]);
+    const [applied, setApplied] = useState(false);
+    const {user} = useContext(UserContext);
+    const dataLoaded = useRef(false);
+    const {enqueueSnackbar} = useSnackbar();
+    const setError = message => enqueueSnackbar(message, {variant: 'error'});
+    const setSuccess = message => enqueueSnackbar(message, {variant: 'success'});
+    const {job, setJob} = props;
+    const classes = useStyles();
+
+    const init = () => {
         get('/jobs').then(({status, data}) => {
             if (status !== 200) return;
-            this.setState({similarJobs: data});
+            setSimilarJobs(data);
         });
     }
 
-    applyToJob(jobId){
-        const { jwt } = this.context.user;
-        post('/student/jobs/apply/' + jobId, {}, jwt).then(({status, message}) => {
+    useEffect(() => {
+        if (dataLoaded.current === false){
+            dataLoaded.current = true;
+            init();
+        }
+    });
+
+    const applyToJob = (jobId) => {
+        post('/student/jobs/apply/' + jobId, {}, user.jwt).then(({status, data}) => {
             if (status === 201){
-                this.setState({applied: true});
+                setApplied(true);
             }else{
-                alert('Something went wrong :(');
-                console.log(message);
+                setError("Cannot apply to job: " + data.message);
             }
         });
     }
 
-    render() {
-        const { classes } = this.props;
-        const {job, setJob} = this.props;
-        if (!job)return (<p>Loading...</p>);
-        const cityName = job.City ? job.City.name : "Los angeles"
-        const countryName = (job.City && job.City.Country) ? job.City.Country.name : "USA"
-
-
-        return (
-            <div>
-                <div className={`${classes.root} ${classes.shadow}`}>
-                    <Grid container spacing={3} alignItems="center">
-                        <Grid
-                            container
-                            item
-                            xs={3}
-                            sm={2}
-                            md={2}
-                            lg={1}
-                            justify="flex-start"
+    if (!job)return (<p>Loading...</p>);
+    const cityName = job.City ? job.City.name : "Los angeles"
+    const countryName = (job.City && job.City.Country) ? job.City.Country.name : "USA"
+    return (
+        <div>
+            <div className={`${classes.root} ${classes.shadow}`}>
+                <Grid container spacing={3} alignItems="center">
+                    <Grid
+                        container
+                        item
+                        xs={3}
+                        sm={2}
+                        md={2}
+                        lg={1}
+                        justify="flex-start"
+                    >
+                        <Avatar className={classes.avatarStyle} />
+                    </Grid>
+                    <Grid item xs={7} sm={7} md={7} lg={8}>
+                        <Typography variant="h5">{job.name}</Typography>
+                        <Typography variant="h6">{job.Company.name}</Typography>
+                    </Grid>
+                    <Grid container xs={2} sm={3} md={3} lg={3} justify="flex-end">
+                        {user.userType === 'company' && <EditOutlinedIcon onClick={() => history.push('/editjob/'+job.id)}/>}
+                        {user.userType === 'student' && <Button
+                            size="medium"
+                            variant="contained"
+                            color="primary"
+                            className={classes.color}
+                            onClick={() => applyToJob(job.id)}
+                            disabled={applied}
                         >
-                            <Avatar className={classes.avatarStyle} />
-                        </Grid>
-                        <Grid item xs={7} sm={7} md={7} lg={8}>
-                            <Typography variant="h5">{job.name}</Typography>
-                            <Typography variant="h6">{job.Company.name}</Typography>
-                        </Grid>
-                        <Grid container xs={2} sm={3} md={3} lg={3} justify="flex-end">
-                            <EditOutlinedIcon onClick={() => history.push('/editjob/'+job.id)}/>
-                            <ModalViewJob job={job} setJob={setJob} />
-                            <Button
-                                size="medium"
-                                variant="contained"
-                                color="primary"
-                                className={classes.color}
-                                onClick={() => this.applyToJob(job.id)}
-                                disabled={this.state.applied}
-                            >
-                                {this.state.applied ? 'Applied!' : 'Apply now'}
-                            </Button>
-                        </Grid>
+                            {applied ? 'Applied!' : 'Apply now'}
+                        </Button>}
                     </Grid>
-
-                    <Grid container spacing={3}>
-                        <Grid container item xs={12} md={12} lg={12} justify="flex-start">
-                            <Grid>
-                                <Typography variant="subtitle1">{cityName} | {countryName} | {job.remote ? 'Remote' : 'In Place'} | {job.salary || 500}€</Typography>
-                                <Grid container>
-                                    <Typography variant="subtitle1">
-                                        Category: IT
-                                    </Typography>
-                                </Grid>
-                                <Typography variant="h6">Description</Typography>
-                                <p>
-                                    {job.description || placeHolderText}
-                                </p>
-                            </Grid>
-
-
-                            <Grid xs={12} item>
-                                <div className={classes.tags} style={{justifyContent: "left"}}>
-                                    {job.requiredSkills.map(s =>
-                                        <Chip key={'skill-required-' + s.id}
-                                              className={classes.tag}
-                                              variant="outlined"
-                                              color="primary"
-                                              label={s.name}
-                                              href="#chip"
-                                              style={{backgroundColor: '#1076a3', color: 'white'}}
-                                        />
-                                    )}
-                                    {job.optionalSkills.map(s =>
-                                        <Chip key={'skill-optional' + s.id}
-                                              className={classes.tag}
-                                              variant="outlined"
-                                              color="secondary"
-                                              label={s.name}
-                                              href="#chip"
-                                        />
-                                    )}
-                                </div>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </div>
-                <Grid container justify="center" lg={12}>
-                    <h2>Similar internships</h2>
                 </Grid>
-                <Carousel breakPoints={breakPoints} className={classes.topMargin}>
-                    {this.state.similarJobs.map(j => <Card key={'similarJob-'+j.id} job={j} discardJob={() => {}} />)}
-                </Carousel>
-            </div>
-        );
-    }
-}
 
-export default withStyles(useStyles)(CardCarousel)
+                <Grid container spacing={3}>
+                    <Grid container item xs={12} md={12} lg={12} justify="flex-start">
+                        <Grid>
+                            <Typography variant="subtitle1">{cityName} | {countryName} | {job.remote ? 'Remote' : 'In Place'} | {job.salary || 500}€</Typography>
+                            <Grid container>
+                                <Typography variant="subtitle1">
+                                    Category: IT
+                                </Typography>
+                            </Grid>
+                            <Typography variant="h6">Description</Typography>
+                            <p>
+                                {job.description || placeHolderText}
+                            </p>
+                        </Grid>
+
+
+                        <Grid xs={12} item>
+                            <div className={classes.tags} style={{justifyContent: "left"}}>
+                                {job.requiredSkills.map(s =>
+                                    <Chip key={'skill-required-' + s.id}
+                                          className={classes.tag}
+                                          variant="outlined"
+                                          color="primary"
+                                          label={s.name}
+                                          href="#chip"
+                                          style={{backgroundColor: '#1076a3', color: 'white'}}
+                                    />
+                                )}
+                                {job.optionalSkills.map(s =>
+                                    <Chip key={'skill-optional' + s.id}
+                                          className={classes.tag}
+                                          variant="outlined"
+                                          color="secondary"
+                                          label={s.name}
+                                          href="#chip"
+                                    />
+                                )}
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
+            <Grid container justify="center" lg={12}>
+                <h2>Similar internships</h2>
+            </Grid>
+            <Carousel breakPoints={breakPoints} className={classes.topMargin}>
+                {similarJobs.map(j => <Card key={'similarJob-'+j.id} job={j} discardJob={() => {}} />)}
+            </Carousel>
+        </div>
+    );
+}
