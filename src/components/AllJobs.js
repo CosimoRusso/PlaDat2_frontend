c12235c05cfd6d2dbcebd64d9e114e7cf7c71f0d
Cosimo Russo <cosimo.russo123@gmail.com>
2021-02-23 19:04:39 +0100

    Remove "delete" button in browse all jobs page

--- src/components/AllJobs.js
+++ src/components/AllJobs.js
@@ -7,10 +7,8 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import ClearIcon from '@material-ui/icons/Clear';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 
@@ -27,7 +25,7 @@ const useStyles = theme => ({
     background: '#03a9f4',
     '&:hover': {
       background: "#1076a3",
-   },
+    },
   },
 
   avatar: {
@@ -42,10 +40,10 @@ class JobsList extends React.Component {
 
   componentDidMount() {
     axios.get(`https://pladat2.herokuapp.com/api/v1/jobs/`)
-      .then(res => {
-        const jobs = res.data;
-        this.setState({ jobs });
-      })
+        .then(res => {
+          const jobs = res.data;
+          this.setState({ jobs });
+        })
   }
 
 
@@ -53,67 +51,62 @@ class JobsList extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
+        <div>
           <Grid
-          container
-          direction="row"
-          justify= "center"
-        >
-        {this.state.jobs.map((job) => (
+              container
+              direction="row"
+              justify= "center"
+          >
+            {this.state.jobs.map((job) => (
 
-          <Grid item lg={3}>
-          <Card key={'job-'+job.id} className={classes.root}>
-            <Grid item xs>
-              <CardHeader
-                avatar={
-                  <Avatar
-                    aria-label="recipe"
-                    className={classes.avatar}
-                  ></Avatar>
-                }
-                action={
-                  <IconButton aria-label="clear">
-                    <ClearIcon />
-                  </IconButton>
-                }
-                title={job.name}
-                subheader={job.Company.name}
-              />
+                <Grid item lg={3}>
+                  <Card key={'job-'+job.id} className={classes.root}>
+                    <Grid item xs>
+                      <CardHeader
+                          avatar={
+                            <Avatar
+                                aria-label="recipe"
+                                className={classes.avatar}
+                            ></Avatar>
+                          }
+                          title={job.name}
+                          subheader={job.Company.name}
+                      />
 
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {job.description}
-                </Typography>
-              </CardContent>
+                      <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                          {job.description}
+                        </Typography>
+                      </CardContent>
 
-              <CardActions className={classes.left}>
-                <Grid container direction="row" justify="flex-start">
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {job.salary}€ | {job.remote ? 'Remote' : 'In place'}
-                  </Typography>
+                      <CardActions className={classes.left}>
+                        <Grid container direction="row" justify="flex-start">
+                          <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                          >
+                            {job.salary}€ | {job.remote ? 'Remote' : 'In place'}
+                          </Typography>
+                        </Grid>
+                        <Grid container direction="row" justify="flex-end">
+                          <Button
+                              size="small"
+                              variant="contained"
+                              color="primary"
+                              onClick={() => history.push("/viewjob/" + job.id)}
+                              className={classes.color}
+                          >
+                            Read more
+                          </Button>
+                        </Grid>
+                      </CardActions>
+                    </Grid>
+                  </Card>
                 </Grid>
-                <Grid container direction="row" justify="flex-end">
-                  <Button
-                    size="small"
-                    variant="contained"
-                    color="primary"
-                    onClick={() => history.push("/viewjob/" + job.id)}
-                    className={classes.color}
-                  >
-                    Read more
-                  </Button>
-                </Grid>
-              </CardActions>
-            </Grid>
-          </Card>
+            ))}
           </Grid>
-        ))}
-                  </Grid>
-      </div>
+        </div>
     );}
 }
 
